88d14e6eed8bdda82c45e6950dcaaf46b1d08afc
madeeryu <142758683+madeeryu@users.noreply.github.com>
2024-08-22 20:23:16 +0700

    first commit

---
+++ riset/riset_2/percobaan.py
@@ -0,0 +1,92 @@
+import cv2
+import mediapipe as mp
+import math
+import pyfirmata2
+import numpy as np
+
+# here we are informing pyfirmata which port to use
+my_port = 'COM5'
+board = pyfirmata2.Arduino(my_port)
+iter8 = pyfirmata2.util.Iterator(board)
+iter8.start()
+
+# pin number of our servo motor is 9
+pin9 = board.get_pin('d:9:s')
+
+# the following three lines are to help us change the colors of our finger tips and line joining them in mediapipe library
+mp_drawing = mp.solutions.drawing_utils
+hand_mpDraw = mp.solutions.drawing_utils
+mp_hands = mp.solutions.hands
+
+# the motor function will send the instruction to our pyfirmata library which will send it to our arduino
+def move_servo(angle):
+    pin9.write(angle)
+
+# Since we cannot create a dotted line in openCV directly, I wrote a function that will take two points and create a dotted line between them.
+# we are using this dotted line to adjust the intensity of light
+def drawline(img, pt1, pt2, color, thickness=1, style='dotted', gap=20):
+    dist = ((pt1[0] - pt2[0]) ** 2 + (pt1[1] - pt2[1]) ** 2) ** .5
+    pts = []
+    for i in np.arange(0, dist, gap):
+        r = i / dist
+        x = int((pt1[0] * (1 - r) + pt2[0] * r) + .5)
+        y = int((pt1[1] * (1 - r) + pt2[1] * r) + .5)
+        p = (x, y)
+        pts.append(p)
+    if style == 'dotted':
+        for p in pts:
+            cv2.circle(img, p, thickness, color, -1)
+    else:
+        s = pts[0]
+        e = pts[0]
+        i = 0
+        for p in pts:
+            s = e
+            e = p
+            if i % 2 == 1:
+                cv2.line(img, s, e, color, thickness)
+            i += 1
+
+cap = cv2.VideoCapture(1)
+# distance is a variable that we will use later in our code. But we must initiate it before our while loop. so I am providing it with a garbage value
+distance = -19723086135
+with mp_hands.Hands(
+        min_detection_confidence=0.5,
+        min_tracking_confidence=0.5) as hands:
+    while cap.isOpened():
+        success, image = cap.read()
+        image = cv2.flip(image, 1)
+        image = cv2.cvtColor(image, cv2.COLOR_BGR2RGB)
+        image.flags.writeable = False
+        results = hands.process(image)
+        image.flags.writeable = True
+        image = cv2.cvtColor(image, cv2.COLOR_RGB2BGR)
+        exit_x, exit_y = 700, 100
+        exit_w, exit_h = 400, 100
+        # The next line is used to create a rectangle with x,y and w,h coordinates
+        cv2.rectangle(image, (exit_x, exit_y), (exit_x + exit_w, exit_y + exit_h), (255, 0, 255), cv2.FILLED)
+        # The next line will put some text on our image
+        cv2.putText(image, "Join your index and middle fingers to exit", (exit_x + 30, exit_y + 65), cv2.FONT_HERSHEY_PLAIN, 2, (255, 255, 255), 2)
+        if results.multi_hand_landmarks:
+            for hand_landmarks in results.multi_hand_landmarks:
+                lmList = []
+                for id, lm in enumerate(hand_landmarks.landmark):
+                    h, w, c = image.shape
+                    cx, cy = int(lm.x * w), int(lm.y * h)
+                    lmList.append([id, cx, cy])
+                    tips = [0, 4, 8, 12, 16, 20]
+                    if id in tips:
+                        cv2.circle(image, (cx, cy), 15, (255, 255, 255), cv2.FILLED)
+                drawline(image, (lmList[4][1], lmList[4][2]), (lmList[8][1], lmList[8][2]), (255, 255, 255), thickness=1, style='dotted', gap=10)
+                
+                # Gerakkan motor servo berdasarkan posisi x dari tangan
+                hand_x = lmList[9][1]  # Menggunakan titik tengah tangan (id 9)
+                servo_angle = np.interp(hand_x, [0, w], [0, 180])  # Interpolasi posisi x ke sudut servo
+                move_servo(servo_angle)
+                print(f"Servo Angle: {servo_angle}")
+
+                mp_drawing.draw_landmarks(image, hand_landmarks, mp_hands.HAND_CONNECTIONS, landmark_drawing_spec=hand_mpDraw.DrawingSpec(color=(0, 0, 0)), connection_drawing_spec=hand_mpDraw.DrawingSpec(color=(201, 194, 2)))
+        cv2.imshow('MediaPipe Hands', image)
+        cv2.waitKey(1)
+cap.release()
+cv2.destroyAllWindows()
